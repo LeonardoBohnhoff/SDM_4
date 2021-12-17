8c6d6bbc9cd57676fa643772354dc82969d154e8
boris-96@hotmail.com <boris-96@hotmail.com>
2021-12-17 22:17:41 +0100

    Deleted tests, added our own

--- psjava/src/main/java/org/psjava/ds/deque/DoubleLinkedList.java
+++ psjava/src/main/java/org/psjava/ds/deque/DoubleLinkedList.java
@@ -2,58 +2,111 @@ package org.psjava.ds.deque;
 
 import java.util.Iterator;
 
+import ch.usi.si.codelounge.jsicko.Contract;
 import org.psjava.ds.PSCollection;
 import org.psjava.util.IterableToString;
 import org.psjava.util.ReadOnlyIterator;
 
-public class DoubleLinkedList<T> implements PSDeque<T>, PSCollection<T> {
+import static ch.usi.si.codelounge.jsicko.Contract.old;
+import static ch.usi.si.codelounge.jsicko.ContractUtils.implies;
+
+public class DoubleLinkedList<T> implements PSDeque<T>, PSCollection<T>, Contract {
 
     public static <T> DoubleLinkedList<T> create() {
         return new DoubleLinkedList<T>();
     }
 
-    private class Node {
-        public T v;
-        public Node n;
-        public Node p;
-
-        public Node(T v, Node p, Node n) {
-            this.v = v;
-            this.p = p;
-            this.n = n;
-        }
-    }
-
-    private final Node head;
-    private final Node tail;
+    private final Node<T> head;
+    private final Node<T> tail;
 
     public DoubleLinkedList() {
-        head = new Node(null, null, null);
-        tail = new Node(null, null, null);
+        head = new Node<T>(null, null, null);
+        tail = new Node<T>(null, null, null);
         head.n = tail;
         tail.p = head;
     }
 
     @Override
+    @Pure
+    @Ensures("check_emptiness")
+    public boolean isEmpty() {
+        return head.n == tail;
+    }
+
+    @Override
+    @Pure
+    @Ensures("check_positivity")
+    public int size() {
+        int r = 0;
+        Node<T> node = head;
+        while (node.n != tail) {
+            r++;
+            node = node.n;
+        }
+        return r;
+    }
+
+    @Override
+    @Ensures("check_references_at_head_after_insertion")
+    public void addToFirst(T v) {
+        add(head.n, v);
+    }
+
+    @Override
+    @Ensures("check_references_at_tail_after_insertion")
     public void addToLast(T e) {
         add(tail, e);
     }
 
     @Override
+    @Ensures("is_empty")
     public void clear() {
         head.n = tail;
         tail.p = head;
     }
 
     @Override
-    public boolean isEmpty() {
-        return head.n == tail;
+    @Pure
+    public T getFirst() {
+        return head.n.v;
+    }
+
+    @Override
+    @Pure
+    public T getLast() {
+        return tail.p.v;
+    }
+
+    @Override
+    @Requires("!is_empty")
+    @Ensures("check_references_at_head_after_removal")
+    public T removeFirst() {
+        return remove(head.n);
+    }
+
+    @Override
+    @Requires("!is_empty")
+    @Ensures("check_references_at_tail_after_removal")
+    public T removeLast() {
+        return remove(tail.p);
+    }
+
+    private T remove(Node<T> node) {
+        node.p.n = node.n;
+        node.n.p = node.p;
+        return node.v;
+    }
+
+    private void add(Node<T> node, T v) {
+        Node<T> newNode = new Node<>(v, node.p, node);
+        newNode.p.n = newNode;
+        newNode.n.p = newNode;
     }
 
     @Override
     public Iterator<T> iterator() {
         return new ReadOnlyIterator<T>() {
-            Node next = head.n;
+            Node<T> next = head.n;
 
             @Override
             public boolean hasNext() {
@@ -70,56 +123,60 @@ public class DoubleLinkedList<T> implements PSDeque<T>, PSCollection<T> {
     }
 
     @Override
-    public int size() {
-        int r = 0;
-        Node node = head;
-        while (node.n != tail) {
-            r++;
-            node = node.n;
-        }
-        return r;
-    }
-
-    @Override
-    public void addToFirst(T v) {
-        add(head.n, v);
-    }
-
-    @Override
-    public T getFirst() {
-        return head.n.v;
-    }
-
-    @Override
-    public T getLast() {
-        return tail.p.v;
-    }
-
-    @Override
-    public T removeFirst() {
-        return remove(head.n);
-    }
-
-    @Override
-    public T removeLast() {
-        return remove(tail.p);
-    }
-
-    private T remove(Node node) {
-        node.p.n = node.n;
-        node.n.p = node.p;
-        return node.v;
-    }
-
-    private void add(Node node, T v) {
-        Node newNode = new Node(v, node.p, node);
-        newNode.p.n = newNode;
-        newNode.n.p = newNode;
-    }
-
-    @Override
     public String toString() {
         return IterableToString.toString(this);
     }
 
+    // jSicko additions
+
+    @Invariant
+    @Pure
+    public boolean positive_size() {
+        return size() >= 0;
+    }
+
+    @Invariant
+    @Pure
+    public boolean head_and_tail_pointers() {
+        return this.head.n != null && this.tail.p != null && this.head.p == null && this.tail.n == null;
+    }
+
+    @Pure
+    public boolean check_positivity(int returns) {
+        return returns >= 0;
+    }
+
+    @Pure
+    public boolean is_empty() {
+        return this.head.n == this.tail && this.tail.p == this.head;
+    }
+
+    @Pure
+    public boolean check_emptiness(boolean returns) {
+        return implies(returns == true, () -> is_empty(), () -> !is_empty());
+    }
+
+    @Pure
+    public boolean check_references_at_head_after_insertion(T v) {
+        //return this.head.n.n == old(this).head.n;
+        return this.head == this.head.n.p && this.head.n == this.head.n.n.p;
+    }
+
+    @Pure
+    public boolean check_references_at_tail_after_insertion(T e) {
+        //return this.tail.p.p == old(this).tail.p;
+        return this.tail == this.tail.p.n && this.tail.p == this.tail.p.p.n;
+    }
+
+    @Pure
+    public boolean check_references_at_head_after_removal() {
+        //return this.head.n == old(this).head.n.n ;
+        return this.head == this.head.n.p;
+    }
+
+    @Pure
+    public boolean check_references_at_tail_after_removal() {
+        //return this.tail.p == old(this).tail.p.p;
+        return this.tail == this.tail.p.n;
+    }
 }
--- psjava/src/test/java/org/psjava/ds/deque/DoubleLinkedListTest.java
+++ psjava/src/test/java/org/psjava/ds/deque/DoubleLinkedListTest.java
@@ -1,16 +1,35 @@
 package org.psjava.ds.deque;
 
-import org.junit.Assert;
-import org.junit.Test;
-import org.psjava.ds.deque.DoubleLinkedList;
+import ch.usi.si.codelounge.jsicko.Contract;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class DoubleLinkedListTest {
 
     @Test
-    public void testEmpty() {
-        DoubleLinkedList<String> a = DoubleLinkedList.create();
-        Assert.assertEquals(0, a.size());
-        Assert.assertEquals("()", a.toString());
+    public void emptyDequeTest() throws Throwable {
+        Executable textFixture = () -> {
+            DoubleLinkedList<String> doubleLinkedList = new DoubleLinkedList<>();
+            doubleLinkedList.size();
+            doubleLinkedList.isEmpty();
+            doubleLinkedList.removeFirst();
+        };
+        assertThrows(Contract.PreconditionViolation.class, textFixture);
+    }
+
+    @Test
+    public void filledDequeTest() throws Throwable {
+        DoubleLinkedList<String> doubleLinkedList = new DoubleLinkedList<>();
+        doubleLinkedList.addToFirst("a");
+        doubleLinkedList.addToFirst("b");
+        doubleLinkedList.addToLast("c");
+        doubleLinkedList.addToLast("d");
+        doubleLinkedList.removeFirst();
+        doubleLinkedList.removeLast();
+        doubleLinkedList.size();
+        doubleLinkedList.isEmpty();
+        doubleLinkedList.clear();
     }
 
 }
